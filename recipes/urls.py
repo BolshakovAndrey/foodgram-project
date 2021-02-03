e6ea59d91c1380ac40c0dfa542664c7f63e319a6
BolshakovAndrey <abolshakovy@gmail.com>
2021-02-03 22:11:09 +0300

    Updated urls after codereview_1

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,25 +1,32 @@
 from django.urls import path
 
-from recipes import views
-from recipes.views import (FavoriteView, GetIngredientsView, PurchasesView,
-                           SubscribeView, purchaselist_download)
+from recipes.views import (AuthorRecipeListView, FavoriteListView,
+                           FavoriteView, FollowListView, GetIngredientsView,
+                           PurchaseList, PurchasesView, RecipeCreateView,
+                           RecipeDeleteView, RecipeDetailView, RecipeListView,
+                           RecipeUpdateView, SubscribeView,
+                           purchaselist_download)
 
 urlpatterns = [
-    path('', views.RecipeListView.as_view(), name='index'),
-    path("<int:pk>/edit/", views.RecipeUpdateView.as_view(), name="edit_recipe"),
-    path("<int:pk>/", views.RecipeDetailView.as_view(), name="recipe"),
-    path("<int:pk>/delete/", views.RecipeDeleteView.as_view(), name="delete_recipe"),
-    path('new', views.RecipeCreateView.as_view(), name='new_recipe'),
+    # recipes
+    path('', RecipeListView.as_view(), name='index'),
+    path('<int:pk>/edit/', RecipeUpdateView.as_view(), name='edit_recipe'),
+    path('<int:pk>/', RecipeDetailView.as_view(), name='recipe'),
+    path('<int:pk>/delete/', RecipeDeleteView.as_view(), name='delete_recipe'),
+    path('new/', RecipeCreateView.as_view(), name='new_recipe'),
     path('ingredients/', GetIngredientsView.as_view(), name='get_ingredients'),
-    path('author/<pk>', views.AuthorRecipeListView.as_view(), name='author'),
-    path('favorites/', views.FavoriteListView.as_view(), name='favorites'),
-    path('favorites', FavoriteView.as_view(), name='add_favorite'),
-    path('favorites/<int:recipe_id>', PurchasesView.as_view(), name='remove_favorites'),
-    path('purchaselist/', views.PurchaseList.as_view(), name='purchaselist'),
-    path('purchases', PurchasesView.as_view(), name='purchases'),
+    path('author/<pk>/', AuthorRecipeListView.as_view(), name='author'),
+    # favorites
+    path('favoriteslist/', FavoriteListView.as_view(), name='favorites'),
+    path('favorites/', FavoriteView.as_view(), name='add_favorite'),
+    path('favorites/<int:recipe_id>/', FavoriteView.as_view(), name='remove_favorites'),
+    # purchases
+    path('purchaselist/', PurchaseList.as_view(), name='purchaselist'),
+    path('purchases/', PurchasesView.as_view(), name='add-purchases'),
+    path('purchases/<int:recipe_id>/', PurchasesView.as_view(), name='remove_purchases'),
     path('shoplist/download/', purchaselist_download, name='purchaselist_download'),
-    path('purchases/<int:recipe_id>', PurchasesView.as_view(), name='remove_purchases'),
-    path('subscriptions/', views.FollowListView.as_view(), name='subscriptions'),
-    path('subscriptions', SubscribeView.as_view(), name='add_subscription'),
-    path('subscriptions/<int:author_id>', SubscribeView.as_view(), name='remove_subscriptions')
+    # subscriptions
+    path('subscriptionslist/', FollowListView.as_view(), name='subscriptions'),
+    path('subscriptions/', SubscribeView.as_view(), name='add_subscription'),
+    path('subscriptions/<int:author_id>/', SubscribeView.as_view(), name='remove_subscriptions')
 ]
