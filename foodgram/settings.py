3d349a52a53c3252a6952ab3185d91e443f76cec
BolshakovAndrey <abolshakovy@gmail.com>
2021-02-08 00:19:54 +0300

    Docker login.io

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -81,8 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
