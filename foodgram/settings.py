dfb2986d4bfc6ef0aa877115bb8471d1e99418e3
BolshakovAndrey <abolshakovy@gmail.com>
2021-02-09 21:04:45 +0300

    Updated infrastructure3-2

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = '42+5=12ql#3057h%i@df_7u(_q03=uf5e)m#!4rxwci$8#4e4t'
 
 if str(BASE_DIR) == '/code':
     DEBUG = False
@@ -9,7 +10,6 @@ else:
     DEBUG = True
 
 if not DEBUG:
-    SECRET_KEY = os.getenv('SECRET_KEY')
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.154.235.71']
 
     DATABASES = {
@@ -23,7 +23,6 @@ if not DEBUG:
         }
     }
 else:
-    SECRET_KEY = '42+5=12ql#3057h%i@df_7u(_q03=uf5e)m#!4rxwci$8#4e4t'
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.154.235.71']
     DATABASES = {
         'default': {
