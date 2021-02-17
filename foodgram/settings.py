3195cd981105d347d192fc3c55093b8c753e363d
BolshakovAndrey <abolshakovy@gmail.com>
2021-02-18 02:22:22 +0300

    Hardcode

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -137,12 +137,20 @@ LOGOUT_REDIRECT_URL = "/auth/login/"
 
 # GMAIL.com
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = False
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_PORT = os.getenv('EMAIL_PORT')
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_SSL = False
+# EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bolshakov.developer@gmail.com'
+EMAIL_HOST_PASSWORD = 'Agava1974'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
